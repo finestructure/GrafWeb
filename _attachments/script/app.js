3f01858a206c4cf89bc1fb121b87e305a201b7f3
Martin Wache <wache@abstracture.de>
2012-02-27 13:24:49 +0100

    - make text_result editable

--- _attachments/script/app.js
+++ _attachments/script/app.js
@@ -36,6 +36,32 @@ $(function() {
         });
     };
     drawItems();
+    // show form, when user clicked on picture or text
+    $("li [class=no-form]").live("click", function(e) {
+        $(this).parents("li").children("[class=no-form]").hide();
+        var form = $(this).parents("li").children("[class=form]");
+        form.show();
+        form.find("[type=text]").focus();
+    });
+    // hide form when text input field no longer has the focus
+    $("li [type=text]").live("blur", function(e) {
+        var li = $(this).parents("li")
+        li.children("[class=form]").hide();
+        li.children("[class=no-form]").show();
+    });
+    // save any changes in the text fields
+    $("li [type=text]").live("change", function(e) {
+        var li = $(this).parents("li")
+        var docid = li.attr("id");
+        var new_text = this.value;
+        db.openDoc(docid, {success : function(doc) {
+            doc.text_result = new_text;
+            db.saveDoc(doc)
+        }});
+    });
+
+
+
     var changesRunning = false;
     function setupChanges(since) {
         if (!changesRunning) {
